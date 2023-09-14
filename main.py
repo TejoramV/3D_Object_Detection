69760f5e5a9ddfd92909489e143b8f2bdd494322
TejoramV <42198973+TejoramV@users.noreply.github.com>
2023-09-14 13:21:43 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import subprocess
 
-num_data = 10
+num_data = 1000
 bounding_box_visualization = False
 for iteration in range(num_data):
     command = ["python", "data_gen.py", "--iteration", str(iteration), "--bounding_box_visualization", str(bounding_box_visualization)]
